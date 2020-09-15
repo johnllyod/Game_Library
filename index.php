32d68230ba54e98cde40f50c87b63abf00a2a3b0
johnllyod <apolo.john017@gmail.com>
2020-09-16 01:02:32 +0800

    Update site

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+</html>
+<html>
+<head>
+	<title>Game Library</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="bootstrap-4.5.2-dist\css\bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body class="bg-dark">
+	<div class="container">
+		<h4 style="display: inline;" class="text-light">GAME LIBRARY</h4>
+		<span class="form-inline float-right">
+			<a href="#" class="mr-3"><h5 class="text-light">Home</h5></a>
+			<a href="#" class="mr-3"><h5 class="text-light">Login</h5></a>
+		</span>
+	</div>
+	<br><br>
+	<div class="container">
+		<h4 class="text-light">GAMES</h4>
+
+		<div class="row">
+			<div class="col-lg-4 col-md-6">
+				<a href="#" class="btn float-left">
+					<img src="images/Bloodborne.png">
+					<h5 class="text-light">Bloodborne</h5>
+				</a>
+			</div>
+			
+			<div class="col-lg-4 col-md-6">
+				<a href="#" class="btn float-left">
+					<img src="images/Dark Souls 2.png">
+					<h5 class="text-light">Dark Souls 2</h5>
+				</a>
+			</div>
+
+			<div class="col-lg-4 col-md-6">
+				<a href="#" class="btn float-left">
+					<img src="images/Cuphead.png">
+					<h5 class="text-light">Cuphead</h5>
+				</a>
+			</div>
+		</div>
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
